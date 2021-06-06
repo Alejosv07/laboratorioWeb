dfeb1e1bd111396481262e0b860d5956ec6a2c74
Alejosv07 <balrking07@gmail.com>
2021-06-05 23:44:51 -0500

    Primer commit

---
+++ WebApplication2/Models/Categoria.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Categoria
+    {
+        //Llave primaria de la tabla categoria
+        [Key]
+        [Display(Name = "Codigo de categoria")]
+        public int idcategoria { set; get; }
+
+        [Display(Name = "Categoria")]
+        [StringLength(50, ErrorMessage = "Campo {0} solo admite entre {1} y {2} caracteres", MinimumLength = 3)]
+        [Required(ErrorMessage = "Campo {0} es requerido")]
+        public string Nombrecategoria { set; get; }
+
+        [Display(Name = "Imagen de categoria")]
+        //[StringLength(50, ErrorMessage = "Campo {0} solo admite una ruta con caracteres entre {1} y {2}", MinimumLength = 3)]
+        //[Required(ErrorMessage = "Campo {0} es requerido")]
+        public string imagenCat { set; get; }
+        [NotMapped]
+        public HttpPostedFileBase ImageFile { get; set; }
+        //Una categoria de juegos puede contener muchos juegos
+        public virtual ICollection<Juego> listaJuegos { set; get; }
+    }
+}
---
+++ WebApplication2/Models/Juego.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Juego
+    {
+        //Llave primaria de la tabla juego
+        [Key]
+        [Display(Name = "Codigo de juego")]
+        public int idjuego { set; get; }
+
+        [Display(Name = "Nombre")]
+        [StringLength(75, ErrorMessage = "Campo {0} solo admite entre {1} y {2} caracteres", MinimumLength = 3)]
+        [Required(ErrorMessage = "Campo {0} es requerido")]
+        public string nomJuego { set; get; }
+
+        [Display(Name = "Categoria")]
+        [Required(ErrorMessage = "Campo {0} es requerido")]
+        public int idcategoria { set; get; }
+
+        [Display(Name = "Precio")]
+        [Required(ErrorMessage = "Campo {0} es requerido")]
+        public float precio { set; get; }
+
+        [Display(Name = "Existencias")]
+        [Required(ErrorMessage = "Campo {0} es requerido")]
+        public int existencias { set; get; }
+
+        [Display(Name = "Imagen")]
+        [StringLength(50, ErrorMessage = "Campo {0} solo admite una ruta con caracteres entre {1} y {2}", MinimumLength = 3)]
+        [Required(ErrorMessage = "Campo {0} es requerido")]
+        public string imagen { set; get; }
+
+        //Relacion entre la tabla Categoria y juego
+        public Categoria categoria { set; get; }
+
+    }
+}
